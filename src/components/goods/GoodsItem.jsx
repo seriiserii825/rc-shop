0ed70ff94af680466b760bbb486f7a5a09b710d5
serii <seriiburduja@gmail.com>
2022-07-11 12:17:46 +0300

    feat: single goods item need to finish

--- src/components/goods/GoodsItem.jsx
+++ src/components/goods/GoodsItem.jsx
@@ -1,3 +1,21 @@
-export const GoodsItem = () => {
-	return <h2>Goods item</h2>;
+export const GoodsItem = (props) => {
+	const { displayName as name, displayDescription as description, displayType as type, price } = props;
+	return (
+		<div className='card'>
+			<div className='card-image'>
+				<img src='images/sample-1.jpg' alt='' />
+				<span className='card-title'>Card Title</span>
+			</div>
+			<div className='card-content'>
+				<p>
+					I am a very simple card. I am good at containing small bits
+					of information. I am convenient because I require little
+					markup to use effectively.
+				</p>
+			</div>
+			<div className='card-action'>
+				<a href='#'>This is a link</a>
+			</div>
+		</div>
+	);
 };
